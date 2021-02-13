65bd6cd3fe0547b80bf87724bdcaa1b881e4844d
gaetanjargot <jargotgaetan@gmail.com>
2021-02-13 17:20:32 -0500

    delete

--- Chapter2/target/debug/build/winapi-0d21be6cc4eab0ec/build_script_build-0d21be6cc4eab0ec.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Gaetan\Documents\GitHub\Rust_book\Rust-Book\Chapter2\target\debug\build\winapi-0d21be6cc4eab0ec\build_script_build-0d21be6cc4eab0ec.exe: C:\Users\Gaetan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs
-
-c:\Users\Gaetan\Documents\GitHub\Rust_book\Rust-Book\Chapter2\target\debug\build\winapi-0d21be6cc4eab0ec\build_script_build-0d21be6cc4eab0ec.d: C:\Users\Gaetan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs
-
-C:\Users\Gaetan\.cargo\registry\src\github.com-1ecc6299db9ec823\winapi-0.3.9\build.rs:
--- Chapter2/target/debug/deps/Chapter2-7b6be9c9c2ff14f1.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Gaetan\Documents\GitHub\Rust_book\Rust-Book\Chapter2\target\debug\deps\Chapter2-7b6be9c9c2ff14f1.rmeta: src\main.rs
-
-c:\Users\Gaetan\Documents\GitHub\Rust_book\Rust-Book\Chapter2\target\debug\deps\Chapter2-7b6be9c9c2ff14f1.d: src\main.rs
-
-src\main.rs:
